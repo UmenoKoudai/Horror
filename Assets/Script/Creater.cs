cd1a843c5ee59d46065abe3e13b07d486c8dfa8e
umenokoudai <umenokoudai0805@gmail.com>
2022-12-12 00:21:57 +0900

    武器設定中

--- Assets/Script/Creater.cs
+++ Assets/Script/Creater.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Creater : MonoBehaviour
 {
-    [SerializeField] Transform[] _positions;
-    [SerializeField] GameObject[] _enemys;
-    [SerializeField] int _intarval;
+    [SerializeField,Header("“G‚ðoŒ»‚³‚¹‚éˆÊ’u")] Transform[] _positions;
+    [SerializeField,Header("oŒ»‚³‚¹‚é“G‚ÌPrefab")] GameObject[] _enemys;
+    [SerializeField,Header("“G‚ðoŒ»‚³‚¹‚éŠÔŠu")] int _intarval;
     float _timer;
 
     void Update()
     {
         _timer += Time.deltaTime;
+        //oŒ»ˆÊ’u‚ðƒ‰ƒ“ƒ_ƒ€‚ÅŽæ“¾
         var randomPosition = Random.Range(0, _positions.Length - 1);
+        //oŒ»‚·‚é“G‚ðƒ‰ƒ“ƒ_ƒ€‚ÅŽæ“¾
         var randomEnemy = Random.Range(0, _enemys.Length - 1);
+        //ˆê’è‚ÌŠÔŠu‚Åƒ‰ƒ“ƒ_ƒ€‚É“G‚ðoŒ»
         if(_timer > _intarval)
         {
             Instantiate(_enemys[randomEnemy], _positions[randomPosition].position, transform.rotation);
