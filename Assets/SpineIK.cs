a3bd899d4fd965b23dd680ae9699ae46e000387a
umenokoudai <umenokoudai0805@gmail.com>
2023-01-07 21:17:36 +0900

    IKを使って向いてる方向に腰を曲げる

---
+++ Assets/SpineIK.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpineIK : MonoBehaviour
+{
+    [SerializeField] Transform _target;
+    [Tooltip("‚Ç‚ê‚­‚ç‚¢Œ©‚é‚©")]
+    [SerializeField, Range(0f, 1f)] float _weight;
+    [Tooltip("‘Ì‚ğ‚Ç‚ê‚­‚ç‚¢Œü‚¯‚é‚©")]
+    [SerializeField, Range(0f, 1f)] float _bodyWeight;
+    [Tooltip("“ª‚ğ‚Ç‚ê‚­‚ç‚¢Œü‚¯‚é‚©")]
+    [SerializeField, Range(0f, 1f)] float _headWeight;
+    [Tooltip("–Ú‚ğ‚Ç‚ê‚­‚ç‚¢Œü‚¯‚é‚©")]
+    [SerializeField, Range(0f, 1f)] float _eyesWeight;
+    [Tooltip("ŠÔÚ‚Ì“®‚«‚ğ‚Ç‚ê‚­‚ç‚¢§ŒÀ‚·‚é‚©")]
+    [SerializeField, Range(0f, 1f)] float _clampWeight;
+    Animator _anim;
+    Vector3 cameraPosition;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        cameraPosition = Camera.main.transform.position;
+        //scameraPosition.z = transform.position.z + 10;
+        cameraPosition = Camera.main.transform.TransformDirection(cameraPosition);
+    }
+    private void OnAnimatorIK(int layerIndex)
+    {
+        _anim.SetLookAtWeight(_weight, _bodyWeight, _headWeight, _eyesWeight, _clampWeight);
+        _anim.SetLookAtPosition(cameraPosition);
+    }
+}
