01d38322391687ca88a92818f3790164da364bde
umenokoudai <umenokoudai0805@gmail.com>
2023-02-26 23:59:04 +0900

    Tree別のやつに変更

---
+++ Assets/Grave.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Grave : EnemyBase
+{
+    [SerializeField] GameObject _crashGrave;
+    [SerializeField] GameObject _keyItem;
+    void Update()
+    {
+        if(base.HP < 0)
+        {
+            Instantiate(_crashGrave, transform.position, transform.rotation);
+            Instantiate(_keyItem, new Vector3(transform.position.x, 0.5f, transform.position.z), transform.rotation);
+            Destroy(gameObject, 0.5f);
+        }
+    }
+}
